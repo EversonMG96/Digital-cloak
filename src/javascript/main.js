cca54bbf5ec1b8d3fa3ba628f9a42eb25593aeaf
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-05 13:09:28 -0300

    create setInnterval add elements

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,8 +2,12 @@ let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
-let currentTime = new Date();
+setInterval(()=>{
+    let currentTime = new Date();
 
 hrs.innerHTML = currentTime.getHours();
 min.innerHTML = currentTime.getMinutes();
 sec.innerHTML = currentTime.getSeconds();
+},1000);
+
+
